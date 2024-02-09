6ea6a66a4404c954904886abd605df4e4ac55119
Jeins Zapata <jeinszapatam@gmail.com>
2024-02-09 10:30:06 -0500

    Estado inicial de luxurSmiles.com

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php include("modulos/head.php") ?>
+<!-- Page Preloder -->
+<div id="preloder">
+    <div class="loader"></div>
+</div>
+
+<!-- Offcanvas Menu Begin -->
+<?php include("modulos/offcanvas.php") ?>
+<!-- Offcanvas Menu End -->
+
+<!-- Header Section Begin -->
+<?php include("modulos/header.php") ?>
+<!-- Header Section End -->
+
+<!-- Hero Section Begin -->
+<?php include("modulos/hero.php") ?>
+<!-- Hero Section End -->
+
+<!-- Consultation Section Begin -->
+<?php include("modulos/consulta.php") ?>
+<!-- Consultation Section End -->
+
+<!-- Chooseus Section Begin -->
+<?php include("modulos/eligenos.php") ?>
+<!-- Chooseus Section End -->
+
+<!-- Services Section Begin -->
+<?php include("modulos/otrosServicios.php") ?>
+<!-- Services Section End -->
+
+<!-- Team Section Begin -->
+<?php include("modulos/casos.php") ?>
+<!-- Team Section End -->
+
+<!-- Gallery Begin -->
+
+<!-- Gallery End -->
+
+<!-- Latest News Begin -->
+<?php include("modulos/noticias.php") ?>
+<!-- Latest News End -->
+
+<!-- Footer Section Begin -->
+<?php include("modulos/footer.php") ?>
+<!-- Footer Section End -->
+
+<!-- Js Plugins -->
+<?php include("modulos/scripts.php") ?>
